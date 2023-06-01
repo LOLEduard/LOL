80382821da9a37f6be478a501a170c70f5d93abb
EduardLOL#4058 <93830780+LOLEduard@users.noreply.github.com>
2023-06-01 22:52:31 +0300

    Update keysystem.lua

--- keysystem.lua
+++ keysystem.lua
@@ -9,7 +9,7 @@ local CoreGui = game:GetService("CoreGui")
 local UserId = game.Players.LocalPlayer.UserId
 local uneverseId = game.GameId
 
-local notif = loadstring(game:HttpGet("https://raw.githubusercontent.com/HoyoGey/My-Lua-Project/main/UiLibs/NotifyLib.lua"))()
+local notif = loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLEduard/LOL/main/notif.lua"))()
 
 local KeyUI = game:GetObjects("rbxassetid://13620051218")[1]
 
